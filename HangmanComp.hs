38c2b0d924559b4076a9803d6403ce8f11d62f85
learese <34835585+learese@users.noreply.github.com>
2019-10-17 02:21:40 -0700

    Added Files

---
+++ HangmanComp.hs
@@ -0,0 +1,140 @@
+-- CPSC 312 - 2019 - Games in Haskell
+module HangmanComp where
+
+-- To run it, try:
+-- ghci
+-- :load Minimax
+
+import Hangman
+import Data.Map    (fromListWith, toList)
+import Data.Char
+
+--------   Determining the best move  ----------
+
+compSmartGuess :: State -> Char
+compSmartGuess (State (letters, g_letters, wbstate, word, guesses) available) =
+  if (elem (fst (getGuess (State (letters, g_letters, wbstate, word, guesses) available))) available)
+    then fst (getGuess (State (letters, g_letters, wbstate, word, guesses) available))
+  else
+    fst (makeLetterGuess (State (letters, g_letters, wbstate, word, guesses) available) letterRank1)
+
+------------------ Letter Ranking -------------------------
+
+letterRank1 :: [(Char, Int)]
+
+letterRank1 = [('A', 5), ('B', 3), ('C', 2), ('D',  3), ('E', 5), ('F', 2), ('G', 2), ('H', 2), ('I', 4), ('J', 1), ('K', 2), ('L', 2), ('M',  2), ('N', 4), ('O', 3), ('P', 3), ('Q', 1), ('R', 5), ('S', 5), ('T' , 4), ('U' , 3), ('V', 1), ('W', 2), ('X', 1), ('Y', 3), ('Z',1) ]
+
+--------- FILTER WORDBANK FOR POTENTIAL MATCHES -----------
+-- removes words that don't match pattern
+
+updateWordBankState :: State -> State
+updateWordBankState (State (letters, g_letters, wbstate, word, guesses) available) = State (letters, g_letters, (filterWordBankLetter wbstate (getLetters letters)), word, guesses) available
+
+
+-- filterWordBankLetter input the current State of the word, output all possible words that match current state
+filterWordBankLetter :: [[Char]] -> [Char]-> [[Char]]
+filterWordBankLetter [] _ = []
+filterWordBankLetter wbstate letters = [word| word <- wbstate, (checkMatch letters word)]
+
+-- checkMatch takes in the current state of the word and a word from the wordbank and returns true if the word is a possible match and false otherwise
+checkMatch :: [Char] -> [Char] -> Bool
+checkMatch [] _ = False
+checkMatch _ [] = False
+checkMatch (h:t) (x:y)
+    | (t == [] && y == []) = if (h == x || h == '_') then True else False
+    | (h == x || h == '_')  = checkMatch t y
+    | otherwise = False
+
+getLetters :: [Char] -> [Char]
+getLetters [] = []
+getLetters (h:t)
+    | h == ' ' = getLetters t
+    | otherwise = (toLower h): getLetters t
+
+
+------------- ALGORITHM FOR DETERMINING COMPUTER'S GUESS --------------
+-- returns the computer's guess
+
+-- getGuess function which returns the Char guess of the computer
+getGuess :: State -> (Char, Int)
+getGuess (State (letters, g_letters, wbstate, word, guesses) available) =
+  do
+    let i = 0
+    let listOfIndexes = getIndexes (getLetters letters) i
+    let listOfGuesses = [getRank wbstate index available | index <- listOfIndexes]  -- get the list of possible letter guesses as a tuple with their rankings i.e ('s', 3)
+    if (listOfGuesses == [])
+      then (makeLetterGuess (State (letters, g_letters, wbstate, word, guesses) available) letterRank1) -- if no guesses, select a valid letter from list of ranked letters
+    else
+     getMaxRank ([getMaxRank guesses available| guesses <- listOfGuesses]) available
+
+-- get the letter with the greatest occurrence in the wordbank at all index positions
+
+
+
+-- try maxOccurrence (getRank (filterWordBank 4 wordbank1) 1)  -- output (s, 34)
+
+-- makeLetterGuess
+makeLetterGuess :: State -> [(Char, Int)] -> (Char, Int)
+makeLetterGuess (State (letters, g_letters, wbstate, word, guesses) available) letterRank =
+  do
+    let count = 0
+    let vowels = [('A', 5), ('E', 5), ('I', 4), ('O', 3), ('U', 3)]
+    let updatedLetterRank = filterLetterRank available letterRank
+    -- if (checkVowels (getLetters letters) count)
+    -- then (getMaxRank [(vowel, rank)|(vowel, rank) <- vowels, (elem vowel available)] available) -- get a vowel as a guess that is still available
+    -- else
+    getMaxRank [(letter, rank) | (letter,  rank) <- updatedLetterRank, (elem letter available)] available
+
+{-
+-- checkVowels
+checkVowels :: [Char] -> Int -> Bool
+checkVowels [] count
+      | count > 1 = False
+      | otherwise = True
+checkVowels (h:t) count =
+    do
+      let vowels = "aeiou"
+      if (elem h vowels)
+        then checkVowels t (count + 1)
+        else checkVowels t count
+-}
+
+-- filterLetterRank input letterRank, output a list of tuples of letters that are still available_actions
+filterLetterRank :: [Char] -> [(Char, Int)] -> [(Char, Int)]
+filterLetterRank  [] _ = []
+filterLetterRank available letterRank = [(letter, rank) | (letter, rank) <- letterRank, (elem letter available)]
+
+
+
+-- getIndexes gets the indexes of all '_' in the current state of the word to be guessed
+getIndexes :: [Char] -> Int -> [Int]
+getIndexes [] _ = []
+getIndexes (h:t) index
+    | h == '_' = ((index+1): getIndexes t (index+1))
+    | otherwise = getIndexes t (index+1)
+
+-- getRank inputs a wordbank and an index to search at for each word in wordbank, at all the empty slots in the letters to be guessed, returns the letter with the most hits
+getRank :: [[Char]] -> Int -> [Char] -> [(Char, Int)]
+getRank wordbank index available =
+  do
+  let currIndexLetters = [ last (take index word) | word <- wordbank]  -- At a given index, return a list of all chars found for each word in wordbank at that given position index
+  let currIndexGuessesRanked = occurrences currIndexLetters available -- for each letter found, if letter has not been guessed and available, then get the frequency of occurence and add to list of Guesses for the Index
+  currIndexGuessesRanked
+
+-- occurrences inputs a list of char letters and outputs a list of tuples (char, frequency) where char is the letter found and frequency is the number of occurences of that letter in letters
+occurrences :: [Char] -> [Char] -> [(Char, Int)]
+occurrences letters available = toList (fromListWith (+) [(letter, 1) | letter <- letters, (elem letter available)])
+
+-- getMaxRank
+getMaxRank :: [(Char, Int)] -> [Char] -> (Char, Int)
+getMaxRank [] available = ((head available) , 0)
+getMaxRank (h:t) _ = maxRest h t
+  where maxRest (a,b) [] = (a,b)
+        maxRest (a,b) (x:y)
+          | ((b <= (snd x)) && (b > 0))  = maxRest x y
+          | otherwise = maxRest (a,b) y
+
+
+-------- Computer Hangman Player -----------------
+comp_hangman_player :: Player
+comp_hangman_player state = compSmartGuess state
---
+++ Play.hs
@@ -0,0 +1,140 @@
+-- CPSC 312 - 2019 - Project 1
+-- Brendan David Chung - 10580744
+-- Leann Santa Cruz - 24928525
+
+module Play where
+
+-- To run it, try:
+-- ghci
+-- :load Play
+
+import Hangman
+import HangmanComp
+import System.IO
+import Text.Read   (readMaybe)
+import Data.Maybe   (fromJust)
+
+type TournammentState = (Int,Int)   -- wins, losses
+
+play :: Game -> State -> Player -> TournammentState -> IO TournammentState
+-- the Player begins a new game.
+play game start_state opponent ts =
+  let (wins, losses) = ts in
+  do
+      putStrLn "\n"
+      putStrLn "This project was developed by Brendan Chung and Leann Santa Cruz based on Hangman."
+      putStrLn "Hangman is a paper and pencil guessing game for two or more players. One player thinks of a word,"
+      putStrLn "phrase or sentence and the other(s) tries to guess it by suggesting letters or numbers,"
+      putStrLn "within a certain number of guesses. (from Wikipedia)"
+      putStrLn "\n"
+      putStrLn ("Tournament Results: "++show wins++ " wins "++show losses++" losses")
+      putStrLn "Press 1 to start guessing."
+      putStrLn "Press 2 to set a word for the computer to guess."
+      putStrLn "Otherwise, press anything else to exit."
+      line1 <- getLine
+      if line1 == "1"
+        then
+          do
+            putStrLn "\n"
+            person_play game (ContinueGame start_state) opponent ts
+      else if line1 == "2"
+        then
+          do
+            putStrLn "\n"
+            putStrLn "Enter a word for the computer to guess."
+            line2 <- getLine
+            computer_play game (ContinueGame (setStartState (remove line2))) opponent ts
+      else
+          do
+            putStrLn "\n"
+            putStrLn "Thanks for playing! Here's the final score. (Wins, Losses)"
+            return ts
+
+
+
+person_play :: Game -> Result -> Player -> TournammentState -> IO TournammentState
+-- the Player guesses a letter in the word. Main body.
+person_play game (EndOfGame val start_state) opponent ts =
+  do
+    newts <- update_tournament_state (val) ts
+    play game start_state opponent newts
+
+person_play game (ContinueGame state) opponent ts =
+   do
+      let State internal avail = state
+      putStrLn(unlines(fullImage(6 - (displayGuessCount state))))
+      putStrLn ("You have " ++show ((displayGuessCount state) + 1)++ " guess(es) left.")
+      putStrLn ("Your word to guess has "++show(getWordLength(state))++" letter(s).")
+      putStrLn (show state)
+      line <- getLine
+      let action = (readMaybe line :: Maybe Char)
+      if (action == Nothing) || not ((fromJust action) `elem` avail)
+        then  -- error; redo
+          do
+           putStrLn "Please type a letter from a to z that you haven't used already. Make sure you add quotation marks around it. e.g. 'a' or 'z'."
+           putStrLn "\n"
+           person_play game (ContinueGame state) opponent ts
+        else if (checkGuess (fromJust action) state)
+          then
+            do
+              putStrLn "You guessed correctly!"
+              putStrLn "\n"
+              person_play game (game (fromJust action) state) opponent ts
+        else
+            do
+              putStrLn "You guessed incorrectly."
+              putStrLn "\n"
+              person_play game (game (fromJust action) state) opponent ts
+
+
+
+computer_play :: Game -> Result -> Player -> TournammentState -> IO TournammentState
+-- computer_play game current_result opponent ts
+-- person entered a word, the computer now guesses that word
+computer_play game (EndOfGame val  start_state) opponent ts =
+   do
+      newts <- update_tournament_state (-val) ts
+      play game start_state opponent newts
+
+computer_play game (ContinueGame state) opponent ts =
+      let
+          newState = updateWordBankState state
+          opponent_move = opponent newState
+        in
+          do
+            putStrLn "\n"
+            putStrLn (show newState)
+            putStrLn "\n"
+            putStrLn(unlines(fullImage(6 - (displayGuessCount newState))))
+            putStrLn ("Number of guess(es) left:  " ++show ((displayGuessCount newState) + 1)++ ".")
+            putStrLn ("The word to guess has "++show(getWordLength(newState))++" letter(s).")
+            putStrLn ("The computer chose "++show opponent_move)
+            if (checkGuess opponent_move newState)
+              then
+                do
+                  putStrLn "The computer guessed correctly!"
+                  putStrLn "\n"
+                  computer_play game (game opponent_move newState) opponent ts
+            else
+              do
+                putStrLn "The computer guessed incorrectly."
+                putStrLn "\n"
+                computer_play game (game opponent_move newState) opponent ts
+
+
+
+update_tournament_state:: Double -> TournammentState -> IO TournammentState
+-- given value a value to the Player, the Tournament State, return the new Tournament State
+update_tournament_state val (wins, losses)
+  | val == 1 = do
+      putStrLn "You guessed the word correctly!"
+      return (wins + 1, losses)
+  | val == -1 = do
+      putStrLn "The computer guessed correctly! You lost."
+      return (wins, losses + 1)
+  | val == 2 = do
+      putStrLn "You did not guess the word correctly."
+      return (wins, losses + 1)
+  | otherwise = do
+      putStrLn "The computer guessed incorrectly. You won!"
+      return (wins + 1, losses)
